0ff80b6998793f05d0051f1750d9b8b78518bef2
lilxfirecracker <52981150+lilxfirecracker@users.noreply.github.com>
2019-07-16 15:50:11 -0700

    Create personal_portfolio1.js

---
+++ personal_portfolio1.js
@@ -0,0 +1,17 @@
+alert("Welcome to my page!");
+
+function danceButton() {
+  window.scrollTo(0, 265); // For Chrome, Firefox, IE and Opera
+}
+
+function skateButton() {
+  window.scrollTo(0, 660); // For Chrome, Firefox, IE and Opera
+}
+
+function modelButton() {
+  window.scrollTo(0, 1080); // For Chrome, Firefox, IE and Opera
+}
+
+function pageantButton() {
+  window.scrollTo(0, 1300); // For Chrome, Firefox, IE and Opera
+}
