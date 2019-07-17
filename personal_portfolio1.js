e7fe56743869a9be9b8568ee24d92099a5ca0bc3
lilxfirecracker <52981150+lilxfirecracker@users.noreply.github.com>
2019-07-17 10:00:57 -0700

    Update personal_portfolio1.js
    fixing buttons yet again

--- personal_portfolio1.js
+++ personal_portfolio1.js
@@ -1,11 +1,11 @@
 alert("Welcome to my page!");
 
 function danceButton() {
-  window.scrollTo(0, 250); // For Chrome, Firefox, IE and Opera
+  window.scrollTo(0, 280); // For Chrome, Firefox, IE and Opera
 }
 
 function skateButton() {
-  window.scrollTo(0, 700); // For Chrome, Firefox, IE and Opera
+  window.scrollTo(0, 900); // For Chrome, Firefox, IE and Opera
 }
 
 function modelButton() {
